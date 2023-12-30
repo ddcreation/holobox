887e0a595df58eac6883c1a6bba4f0f9e91762f5
David Daupeyroux <david.ddcreation@gmail.com>
2023-12-30 16:19:54 +0100

    fix: :bug: Fixing display identifier before launching chromium

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Kill existing chromium
-killall chromium-browser
+killall chromium-browser > /dev/null
 
 # Find Chromium browser process ID
 chromium_pid=""
 
 # Check if Chromium is running
 while [[ -z $chromium_pid ]]; do
+  echo "Chromium still launching..."
+
   sleep 5
   chromium_pid=$(pgrep chromium | head -1)
 done
 
 echo "Chromium browser process ID: $chromium_pid"
 
+export DISPLAY=:0.0
 chromium-browser http://localhost/ http://localhost/clock http://localhost/hologram --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized
--- scripts/switchtab.sh
+++ scripts/switchtab.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-export DISPLAY=:0.0
-
 /usr/bin/xdotool key Ctrl+Shift+Tab
