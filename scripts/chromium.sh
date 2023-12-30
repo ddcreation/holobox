e6cd5130bf7c12c4b804ef5a60d648411ed1fac0
David Daupeyroux <david.ddcreation@gmail.com>
2023-12-30 16:28:40 +0100

    feat: :sparkles: Remove chromium warnings

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -4,4 +4,4 @@
 killall chromium-browser 2> /dev/null
 
 export DISPLAY=:0.0
-chromium-browser http://localhost/ http://localhost/clock http://localhost/hologram --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized
+chromium-browser http://localhost/ http://localhost/clock http://localhost/hologram --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized --disable-extensions
