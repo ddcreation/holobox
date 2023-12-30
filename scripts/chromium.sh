10a4ebbb777e95ddf758d1d102c89276eed87d57
David Daupeyroux <david.ddcreation@gmail.com>
2023-12-30 16:26:33 +0100

    fix: :bug: fix chromium launch

--- scripts/chromium.sh
+++ scripts/chromium.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
 # Kill existing chromium
-killall chromium-browser > /dev/null
-
-# Find Chromium browser process ID
-chromium_pid=""
-
-# Check if Chromium is running
-while [[ -z $chromium_pid ]]; do
-  echo "Chromium still launching..."
-
-  sleep 5
-  chromium_pid=$(pgrep chromium | head -1)
-done
-
-echo "Chromium browser process ID: $chromium_pid"
+killall chromium-browser 2> /dev/null
 
 export DISPLAY=:0.0
 chromium-browser http://localhost/ http://localhost/clock http://localhost/hologram --kiosk --noerrdialogs --disable-infobars --no-first-run --enable-features=OverlayScrollbar --start-maximized
